8e1ef798bfe48e7ff83246938142b315342301d4
BrentonBudler <60913244+BrentonBudler@users.noreply.github.com>
2020-04-28 14:35:51 +0200

    FPS movement
    Creation of basic FPS mechanics including movement and shooting

---
+++ NewPrototype/Assets/Gun.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash; 
+       
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+
+        RaycastHit hitinfo;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hitinfo, range))
+        {
+            Debug.Log(hitinfo.transform.name);
+
+            Enemy enemy = hitinfo.transform.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+        }
+        
+
+    }
+}
